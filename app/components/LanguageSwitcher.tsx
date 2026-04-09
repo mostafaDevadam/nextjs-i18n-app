9947b2f335b1e6add8bb0745b6bfb2775715dadb
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-09 06:57:59 +0200

    fix path when change locale

--- app/components/LanguageSwitcher.tsx
+++ app/components/LanguageSwitcher.tsx
@@ -4,25 +4,45 @@ import { useLocale } from 'next-intl';
 import { useRouter, usePathname, Link } from '@/i18n/navigation';
 import { useParams } from 'next/navigation';
 import { routing } from '@/i18n/routing';
+import { useState } from 'react';
 
 export default function LanguageSwitcher() {
   const currentLocale = useLocale();
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
-    const currentPath = usePathname();
+  const currentPath = usePathname();
+  const [state, setState] = useState<string>(currentLocale);
 
 
   const handleChange = (newLocale: string) => {
     // Option 1: Simple replace (keeps the same path)
     //router.replace(pathname, { locale: newLocale });
-     //router.push(pathname, { locale: newLocale });
+    //router.push(pathname, { locale: newLocale });
     //router.refresh()
+    console.log("newLocale", newLocale)
 
     //if (newLocale === currentLocale) return;
+    setState(newLocale);
+    console.log({newLocale, currentLocale, currentPath, c: currentPath})
+
+    if(currentPath?.split('/')[2] == undefined && currentPath?.split('/')[1] != undefined){
+      console.log("currentPath?.split('/')[2]", currentPath)
+      router.replace("/"+currentPath?.split('/')[1], { locale: newLocale });
+    }
+
+    if(newLocale && currentPath?.split('/')[2] != undefined) {
+      router.replace("/"+currentPath?.split('/')[2], { locale: newLocale });
+    }
+
     
-    router.push(cleanPath, { locale: newLocale });
+
+    //router.push(cleanPath, { locale: newLocale });
+
     
+
+    
+
     // Option 2: If you need to preserve query params
     // const currentPath = pathname;
     // router.replace(currentPath, { locale: newLocale });
@@ -31,16 +51,20 @@ export default function LanguageSwitcher() {
 
   // Remove the current locale from the path to get the clean path
   const getCleanPath = () => {
+    console.log("currentPath", currentPath)
     // Remove /en or /fr from the start of the path
-    const pathWithoutLocale = currentPath.replace(/^\/[a-z]{2}/, '');
+    const pathWithoutLocale = currentPath.replace(/^\/[a-z]{2,3}/, '');
+    console.log("pathWithoutLocale", pathWithoutLocale)
     return pathWithoutLocale || '/';
+    //return pathWithoutLocale.startsWith('/') ? pathWithoutLocale : '/' + pathWithoutLocale;
+
   };
 
-  const cleanPath = getCleanPath();
+  //const cleanPath = getCleanPath();
 
   return (
     <div className="flex gap-2">
-       {/* <Link 
+      {/* <Link 
         href={cleanPath} 
         locale="en"
         style={{ 
@@ -60,36 +84,40 @@ export default function LanguageSwitcher() {
       >
         Français
       </Link>*/}
-      <button
+     {/* <button
         onClick={() => handleChange('en')}
-        className={`px-3 py-1 rounded ${
-          currentLocale === 'en' 
-            ? 'bg-blue-500 text-white' 
+        className={`px-3 py-1 rounded ${currentLocale === 'en'
+            ? 'bg-blue-500 text-white'
             : 'bg-gray-200'
-        }`}
+          }`}
       >
         English
       </button>
       <button
         onClick={() => handleChange('fr')}
-        className={`px-3 py-1 rounded ${
-          currentLocale === 'fr' 
-            ? 'bg-blue-500 text-white' 
+        className={`px-3 py-1 rounded ${currentLocale === 'fr'
+            ? 'bg-blue-500 text-white'
             : 'bg-gray-200'
-        }`}
+          }`}
       >
         Français
       </button>
       <button
         onClick={() => handleChange('ar')}
-        className={`px-3 py-1 rounded ${
-          currentLocale === 'ar' 
-            ? 'bg-blue-500 text-white' 
+        className={`px-3 py-1 rounded ${currentLocale === 'ar'
+            ? 'bg-blue-500 text-white'
             : 'bg-gray-200'
-        }`}
+          }`}
       >
         العربية
-      </button>
+      </button>*/}
+
+      <select value={state} defaultValue={currentLocale} onChange={(e) => handleChange(e.target.value)}>
+        <option value="en">English</option>
+        <option value="fr">French</option>
+        <option value="ar">العربية</option>
+
+      </select>
     </div>
   );
 }
