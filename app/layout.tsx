ae67fb6b5fe40bd5c19af8ea88534ea68c56e84e
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-14 08:58:59 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default async function RootLayout({ children }: Props) {
     <html >
       <body>{children}
         {/*<NextIntlClientProvider >
-         
         </NextIntlClientProvider>*/}
       </body>
     </html>
