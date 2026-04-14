1650263e0d509c2a54b5376ded83aee04ec9c564
mostafaDevadam <adam.mostafa@outlook.de>
2026-04-14 08:48:21 +0200

    update

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['en', 'fr', "ar", "de"],
   defaultLocale: 'en',
-   localePrefix: 'as-needed'
+  localePrefix: 'always'
 });
 
